bfb0fedd723475acceaf3ad5a6250a9dc95020a1
Rodrigo Ferreira <r.ferreira@unoesc.edu.br>
2025-09-22 09:51:28 -0300

    feat: altera mensagem para versão nova

--- src/MinhaApi/Program.cs
+++ src/MinhaApi/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
-app.MapGet("/", () => Results.Ok(new { message = "Minha API .NET 8 rodando!" }));
+app.MapGet("/", () => Results.Ok(new { message = "Minha API .NET 8 – versão NOVA!" }));
+//app.MapGet("/", () => Results.Ok(new { message = "Minha API .NET 8 rodando!" }));
 
 app.Run();
